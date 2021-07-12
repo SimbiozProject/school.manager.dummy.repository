494e1fdd739089021c683902a185a3bf3d4b6f70
zhdanok <zhanokla@gmail.com>
2021-07-13 01:36:07 +0300

    changed beans dependences

--- src/main/java/com/example/web/bean/CourseTable.java
+++ src/main/java/com/example/web/bean/CourseTable.java
@@ -1,20 +1,19 @@
 package com.example.web.bean;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Set;
 
-@Data
 @Builder
 @AllArgsConstructor
 @Entity
+@Getter
+@Setter
 @NoArgsConstructor
 @Table(name = "courses")
+@ToString(exclude = {"userSet", "groupSet"})
 public class CourseTable implements Serializable {
 
     @Id
@@ -24,7 +23,6 @@ public class CourseTable implements Serializable {
 
     @Column(name = "course_name")
     private String courseName;
-
 
     @OneToMany(fetch = FetchType.EAGER, mappedBy = "courseUser")
     private Set<TgUserTable> userSet;
