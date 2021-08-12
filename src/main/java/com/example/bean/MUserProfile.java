e73492c091f80257aa0f6d842c908513a5e6f12d
Maksim Shelkovich <6666350@gmail.com>
2021-08-12 23:24:56 +0300

    Jp 89 new user registration (#7)
    * JP-89: create save new user from tg endpoint
    Co-authored-by: Maksim Shelkovich <info@jrr.by>

--- src/main/java/com/example/bean/MUserProfile.java
+++ src/main/java/com/example/bean/MUserProfile.java
@@ -3,11 +3,20 @@ package com.example.bean;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MUserProfile extends MyEntityM {
     @Id
     @GeneratedValue
-    Long idUserProfile;
-
+    private Long idUserProfile;
+    private Long chatId;
+    private String userName;
 }
--- src/main/java/com/example/repository/MUserProfileRepository.java
+++ src/main/java/com/example/repository/MUserProfileRepository.java
@@ -7,5 +7,4 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource
 public interface MUserProfileRepository extends JpaRepository<MUserProfile, Long> {
 
-
 }
